6843230452499a653fa75fa0c6c4b2d0fd736e06
OGabrielPereira <ogabrielpereira@pm.me>
2022-09-08 14:47:20 -0300

    Setup complited

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className='flex justify-center text-3xl font-bold mt-10 text-white'>TypeNet</h1>
+
+      
+    </div>
+  )
+}
+
+export default Home
