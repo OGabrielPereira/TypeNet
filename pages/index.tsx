10b2d6d2bd46285d254c97a05df56140c6412269
OGabrielPereira <ogabrielpereira@pm.me>
2022-09-08 15:00:46 -0300

    IP Address input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className='flex justify-center text-3xl font-bold mt-10 text-white'>TypeNet</h1>
+    <div >
+      <div className='flex justify-center'>
+        <h1 className=' text-3xl font-bold mt-10 text-white'>TypeNet</h1>
+      </div>
 
-      
+      <div className="flex justify-center gap-2 mt-10">
+          <input
+            type="text"
+            className="flex p-4 pl-6 w-60 text-sm rounded border text-white bg-black"
+            placeholder="Endereço IP"
+            />
+          <button
+            className="bg-black border text-white text-sm rounded flex justify-center items-center p-2.5"
+            >
+        </button>
+
+      </div>
     </div>
   )
 }
